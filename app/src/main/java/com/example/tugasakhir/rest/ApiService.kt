cb23871bd661e898f2a12ca935784e4cff829a65
raditacitra <citraraditha@gmail.com>
2019-06-26 11:04:39 +0700

    first commit

---
+++ app/src/main/java/com/example/tugasakhir/rest/ApiService.kt
@@ -0,0 +1,18 @@
+package com.example.tugasakhir.rest
+
+import com.example.tugasakhir.BuildConfig
+import com.example.tugasakhir.model.RootDataModel
+import retrofit.http.GET
+import rx.Observable
+
+interface ApiService {
+    // http://informatika.upgris.ac.id/mobile/data/index.php/api/example/detil_jadwal?key=33c227f799464dac08f60f1b0d5770&npm=16670025
+    @GET("detil_jadwal?key=" + BuildConfig.API_KEY + "&npm=" + BuildConfig.NPM)
+    fun getJadwalKuliah(
+    ): Observable<RootDataModel>
+
+    @GET("detil_pribadi?key=" + BuildConfig.API_KEY + "&npm=" + BuildConfig.NPM)
+    fun getProfil(
+    ): Observable<RootDataModel>
+
+}
